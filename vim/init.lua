706cff1f4c1033f237061cbdc894e0caf8f096b4
Matt Zhang <set.stun@gmail.com>
2021-07-06 15:27:57 -0400

    Added comments and settings for nvim configs

--- vim/init.lua
+++ vim/init.lua
@@ -5,3 +5,21 @@ set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 ]])
+
+require('nvim-treesitter.configs').setup {
+    ensure_installed = "maintained",  -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ignore_install = { "javascript" },  -- List of parsers to ignore installing
+    highlight = {
+        enable = true,  -- false will disable the whole extension
+        disable = { "c", "rust" },  -- list of language that will be disabled
+    },
+}
+
+require('lspconfig').pyright.setup{}
+
+require('neoscroll').setup{}
+
+vim.opt.termguicolors = true
+require("bufferline").setup{}
+
+require('lualine').setup{}
--- vim/plugins.lua
+++ vim/plugins.lua
@@ -1,39 +1,44 @@
 return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'tmhedberg/SimpylFold'
-    use 'kyazdani42/nvim-tree.lua'
-    use 'godlygeek/tabular'
-    use 'majutsushi/tagbar'
-    use 'scrooloose/nerdcommenter'
-    use 'nvie/vim-flake8'
-    use 'tpope/vim-abolish'
-    use 'p00f/nvim-ts-rainbow'
-    use 'JuliaEditorSupport/julia-vim'
-    use 'stevearc/vim-arduino'
-    use 'vim-test/vim-test'
+    use 'wbthomason/packer.nvim'  -- package manager
+    -- visuals
+    use 'p00f/nvim-ts-rainbow'  -- rainbow parentheses
+    use 'glepnir/indent-guides.nvim'  -- visually display indent levels (*)
+    use 'akinsho/nvim-bufferline.lua'  -- add GUI visuals (*)
     use {
-        'nvim-telescope/telescope.nvim',
-         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+        'hoob3rt/lualine.nvim',  -- beter status line
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}  -- adds icons to plugins (*)
     }
-    use 'sharkdp/bat'
-    use 'sharkdp/fd'
-    use 'BurntSushi/ripgrep'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'neovim/nvim-lspconfig'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'kabouzeid/nvim-lspinstall'
-    use 'hrsh7th/nvim-compe'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'tpope/vim-commentary'
-    use 'voldikss/vim-floaterm'
-    use 'windwp/nvim-autopairs'
-    use 'easymotion/vim-easymotion'
-    use 'karb94/neoscroll.nvim'
-    use 'glepnir/indent-guides.nvim'
+    -- LSP
+    use 'neovim/nvim-lspconfig'  -- automatic launching of LSP servers (*)
+    use 'kabouzeid/nvim-lspinstall'  -- adds "LspInstall command for nvim-lspconfig
     use 'glepnir/lspsaga.nvim'
     use 'onsails/lspkind-nvim'
-    use 'akinsho/nvim-bufferline.lua'
-    use 'hoob3rt/lualine.nvim'
-    use 'hrsh7th/vim-vsnip'
-    use 'simrat39/symbols-outline.nvim'
+    use 'hrsh7th/vim-vsnip' -- LSP-based snippet completion (*)
+    -- code navigation
+    use 'simrat39/symbols-outline.nvim'  -- code tree view sidebar (*)
+    use 'kyazdani42/nvim-tree.lua'  -- file explorer (*)
+    use 'nvim-treesitter/nvim-treesitter'  -- improves interface for nvim-tree and adds functionality (*)
+    use 'majutsushi/tagbar' -- code tree viewer sidebar (*)
+    use 'tmhedberg/SimpylFold'  -- code folding for Python
+    use 'karb94/neoscroll.nvim'  -- smooth window scrolling
+    use 'jremmen/vim-ripgrep'  -- allows using ripgrep inside vim
+    use {
+        'nvim-telescope/telescope.nvim',  -- file search and grep
+         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    }
+    -- other packages
+    use 'godlygeek/tabular'  -- lines stuff up using whitespace (*)
+    use 'scrooloose/nerdcommenter'  -- quickly comment and uncomment code
+    use 'tpope/vim-commentary'  -- should decide whether to use this or nerdcommenter (*)
+    use 'nvie/vim-flake8'  -- PEP8 linter using flake8
+    use 'tpope/vim-abolish'  -- improved search & replace
+    use 'JuliaEditorSupport/julia-vim'  -- LaTeX to Unicode for Julia
+    use 'stevearc/vim-arduino'  -- Arduino functions
+    use 'vim-test/vim-test' -- allows quick unit testing via key bindings
+    use 'sharkdp/bat'
+    use 'sharkdp/fd'
+    use 'hrsh7th/nvim-compe'  -- autocompletion for nvim (*)
+    use 'voldikss/vim-floaterm'  -- open floating terminal window in vim (*)
+    use 'windwp/nvim-autopairs'  -- autocomplete brackets (*)
+    use 'easymotion/vim-easymotion'  -- makes certain vim commands faster to execute (*)
 end)
