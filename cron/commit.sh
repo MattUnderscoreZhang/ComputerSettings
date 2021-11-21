6a9073f0ec6db4b7761bc61ddad8986b781c3388
Matt Zhang <set.stun@gmail.com>
2021-11-21 14:52:04 -0600

    Daily commit

---
+++ cron/commit.sh
@@ -0,0 +1,24 @@
+cd ~/Projects/Cambion/
+git add *
+git commit -m "Daily commit"
+git pull
+git push
+
+cd ~/Projects/VimNotes/
+git add *
+git commit -m "Daily commit"
+git pull
+git push
+
+cd ~/Projects/ComputerSettings/
+rm -rf .config/
+cp -rf ~/.config .
+cp ~/.gitconfig git/
+cp ~/.ssh/config ssh/
+cp ~/Library/Application\ Support/lazygit/config.yml terminal/
+cp ~/.config/nvim/init.lua vim/
+cp ~/.config/nvim/lua/plugins.lua vim/
+git add *
+git commit -m "Daily commit"
+git pull
+git push
