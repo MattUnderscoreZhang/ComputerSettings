9fc2e70842f5829fab334bc86d3fed4a1d36eb4c
Matt Zhang <set.stun@gmail.com>
2021-11-21 16:13:27 -0600

    Daily commit

--- cron/commit.sh
+++ cron/commit.sh
@@ -18,11 +18,11 @@ fi
 
 cd ~/Projects/ComputerSettings/
 rsync -r ~/.config .
-cp -rf ~/.gitconfig git/
-cp -rf ~/.ssh/config ssh/
-cp -rf ~/Library/Application\ Support/lazygit/config.yml terminal/
-cp -rf ~/.config/nvim/init.lua vim/
-cp -rf ~/.config/nvim/lua/plugins.lua vim/
+rsync -r ~/.gitconfig git/
+rsync -r ~/.ssh/config ssh/
+rsync -r ~/Library/Application\ Support/lazygit/config.yml terminal/
+rsync -r ~/.config/nvim/init.lua vim/
+rsync -r ~/.config/nvim/lua/plugins.lua vim/
 git add .
 git commit -m "Daily commit"
 git pull
