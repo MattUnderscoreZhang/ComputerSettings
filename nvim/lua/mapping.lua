6232b3b2d14a40b3089ffd4d4a7a7ca16769f820
Matt Zhang <set.stun@gmail.com>
2023-02-10 00:02:29 -0500

    Daily commit

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -40,6 +40,7 @@ map("n", "<leader>pc", "<cmd>PackerClean<cr>", options)
 -- lsp
 map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", options)  -- go to definition
 map("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", options)  -- go to references
+map("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<CR>", options)  -- info about object
 --map("n", "gb", "<c-o>", options)  -- go to references
 map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", options)  -- rename object
 -- vim-autoformat
