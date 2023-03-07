e0ef2a7ff64ffd4aaef8909da4155d0638947acf
Matt Zhang <set.stun@gmail.com>
2023-03-07 13:27:28 -0500

    Syncing settings across computers

--- cron/commit.sh
+++ cron/commit.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 cd ~/Projects/ComputerSettings/
-#rsync -r ~/.config .
-#rsync -r ~/.gitconfig git/
-#rsync -r ~/.ssh/config ssh/
+rsync -r ~/.config .
+rsync -r ~/.gitconfig git/
+rsync -r ~/.ssh/config ssh/
 rsync -r ~/Library/Application\ Support/lazygit/config.yml terminal/
 rsync -r ~/.config/kitty/ terminal/kitty/
 rsync -r ~/.zshrc terminal/zsh/
 rsync -r ~/.config/nvim/init.lua nvim/
 rsync -r ~/.config/nvim/lua nvim/
-rsync -r ~/.tmux.conf tmux/
-rsync -r ~/.config/zathura/zathurarc zathura/
 git add .
 git commit -m "Daily commit"
 git pull
