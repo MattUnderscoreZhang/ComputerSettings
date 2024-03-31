e91883285e29b30b1bdd35809937407540c33d16
Matt Zhang <set.stun@gmail.com>
2024-03-31 00:00:01 -0400

    Daily commit

--- nvim/lua/plugins/visuals.lua
+++ nvim/lua/plugins/visuals.lua
@@ -7,6 +7,7 @@ M.setup = function(use)
     use 'junegunn/limelight.vim'  -- highlight current paragraph
     use 'preservim/vim-indent-guides'  -- visually display indent levels
     use 'godlygeek/tabular'  -- lines stuff up using whitespace
+    use 'folke/noice.nvim'  -- window in corner replaces other kinds of notifications
 
     require("rainbow-delimiters.setup").setup {}
 
@@ -37,6 +38,25 @@ M.setup = function(use)
         tabline = {},
         extensions = {}
     }
+
+    --require("noice").setup({
+        --lsp = {
+            ---- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            --override = {
+                --["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                --["vim.lsp.util.stylize_markdown"] = true,
+                --["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            --},
+        --},
+        ---- you can enable a preset for easier configuration
+        --presets = {
+            --bottom_search = true, -- use a classic bottom cmdline for search
+            --command_palette = true, -- position the cmdline and popupmenu together
+            --long_message_to_split = true, -- long messages will be sent to a split
+            --inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            --lsp_doc_border = false, -- add a border to hover docs and signature help
+        --},
+    --})
 
     --vim-indent-guides
     vim.g.indent_guides_enable_on_vim_startup = 1
